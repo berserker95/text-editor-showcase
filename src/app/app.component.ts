4704499b54469d19d76ffcb0ce62ecc05cdf3efb
ALBANO GIOVANNI <giovanni.albano1@esternibisp.com>
2024-10-15 17:27:32 +0200

    #002: add syncfusion rich text editor and adjust style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgxEditorComponent } from './components/ngx-editor/ngx-editor.component';
 import { QuillEditorComponent } from './components/quill-editor/quill-editor.component';
+import { SyncfusionTextEditorComponent } from './components/syncfusion-text-editor/syncfusion-text-editor.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, QuillEditorComponent, NgxEditorComponent],
+  imports: [
+    RouterOutlet,
+    QuillEditorComponent,
+    NgxEditorComponent,
+    SyncfusionTextEditorComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,4 +20,5 @@ export class AppComponent {
   title = 'text-editor-showcase';
   editorContent: string = '';
   ngxEditorContent: string = '';
+  syncEditorContent: string = '';
 }
